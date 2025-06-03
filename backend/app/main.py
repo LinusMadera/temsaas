f8f4cf00bcff7d409b213f3010af1a104d4e8d92
LinusMadera <linusmadera@gmail.com>
2025-06-03 12:48:38 -0300

    Change default port and add shadcn

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,4 +48,4 @@ app.include_router(data.router, prefix="/data", tags=["Data"])
 app.include_router(protected.router, prefix="/test", tags=["Test (Restricted Routes)"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8005, reload=True)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8005',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
